2632e0ec112ac80991e1246a8289a2d97261d07e
Aminofa70 <amin.alibakhshi@upm.es>
2026-04-29 13:13:55 +0200

    my first commit

--- README.md
+++ README.md
@@ -4,3 +4,12 @@
 [![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://Aminofa70.github.io/InTriGeom.jl/dev/)
 [![Build Status](https://github.com/Aminofa70/InTriGeom.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/Aminofa70/InTriGeom.jl/actions/workflows/CI.yml?query=branch%3Amain)
 [![Coverage](https://codecov.io/gh/Aminofa70/InTriGeom.jl/branch/main/graph/badge.svg)](https://codecov.io/gh/Aminofa70/InTriGeom.jl)
+
+
+
+This code is the Julia version of 
+Johannes Korsawe (2026). intriangulation(vertices,faces,testp,heavytest) (https://es.mathworks.com/matlabcentral/fileexchange/43381-intriangulation-vertices-faces-testp-heavytest), MATLAB Central File Exchange. Recuperado 29 abril, 2026.
+
+The core algorithm is 99% identical to the algorithm from Adam Aitkenhead.
+
+
---
+++ src/utils.jl
@@ -0,0 +1,35 @@
+"""
+intriangulation: Test points in 3d wether inside or outside a (closed) triangulation
+usage: in = intriangulation(vertices,faces,testp,heavytest)
+
+arguments: (input)
+vertices   - points in 3d as matrix with three columns
+
+faces      - description of triangles as matrix with three columns.
+               Each row contains three indices into the matrix of vertices
+               which gives the three cornerpoints of the triangle.
+
+  testp      - points in 3d as matrix with three columns
+
+  heavytest  - int n >= 0. Perform n additional randomized rotation tests.
+
+ IMPORTANT: the set of vertices and faces has to form a watertight surface!
+
+arguments: (output)
+  in - a vector of length size(testp,1), containing 0 and 1.
+       in(nr) =  0: testp(nr,:) is outside the triangulation
+       in(nr) =  1: testp(nr,:) is inside the triangulation
+       in(nr) = -1: unable to decide for testp(nr,:) 
+
+Thanks to Adam A for providing the FEX submission voxelise. The
+algorithms of voxelise form the algorithmic kernel of intriangulation.
+
+Thanks to Sven to discussions about speed and avoiding problems in
+special cases.
+
+Author: Johannes Korsawe, heavily based on voxelise from Adam A.
+E-mail: johannes.korsawe@volkswagen.de
+Release: 1.3
+Release date: 25/09/2013
+"""
+
